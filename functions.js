68e4f19165a81e9245cfbab8325d7dad923ac4a7
John Artuz <johnartuz@hotmail.com>
2021-11-05 18:06:25 +0000

    Javascripting completion for objects.js, object-properties.js, and functions.js.

---
+++ functions.js
@@ -0,0 +1,6 @@
+function eat(food) {
+  return food + " tasted really good.";
+}
+console.log(eat("bananas"));
+
+git add introduction.js variables.js string.js string-length.js revising-strings.js number.js rounding-numbers.js number-to-string.js for-loop.js array.js array-filtering.js looping-through-arrays.js
